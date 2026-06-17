0a39f208e9ccf38e02be77e78df46322625efece
Happy-skills <happy@johnatan.ru>
2026-06-17 10:56:39 +0300

    feat(core): add env variables

--- internal/config/flag.go
+++ internal/config/flag.go
@@ -1,29 +1,89 @@
 package config
 
-import "flag"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type AgentOptions struct {
-	ServerAddr     string
-	ReportInterval int
-	PollInterval   int
+	ServerAddr     string `env:"ADDRESS,required"`
+	ReportInterval int    `env:"REPORT_INTERVAL,required"`
+	PollInterval   int    `env:"POLL_INTERVAL,required"`
 }
 
 type ServerOptions struct {
-	ServerAddr string
+	ServerAddr string `env:"ADDRESS,required"`
 }
 
 func ParseAgentFlags() AgentOptions {
 	var options AgentOptions
-	flag.StringVar(&options.ServerAddr, "a", "localhost:8080", "address and port server")
-	flag.IntVar(&options.ReportInterval, "r", 10, "interval in seconds for sending metrics")
-	flag.IntVar(&options.PollInterval, "p", 2, "interval in seconds fof getting metrics")
-	flag.Parse()
+
+	if err := env.Parse(&options); err != nil {
+		if errors.Is(err, env.ParseError{}) {
+			log.Printf("can't parse agent environment variables: %s", err.Error())
+		}
+		if errors.Is(err, env.VarIsNotSetError{}) {
+			aggErr := env.AggregateError{}
+			if ok := errors.As(err, &aggErr); ok {
+				for _, er := range aggErr.Errors {
+					switch v := er.(type) {
+					case env.VarIsNotSetError:
+						setAgentFlagByName(&options, v.Key)
+					default:
+						fmt.Printf("Unknown error type %v", v)
+					}
+				}
+			}
+			flag.Parse()
+		}
+	}
+
 	return options
+}
+
+func setAgentFlagByName(opt *AgentOptions, name string) {
+	switch name {
+	case "ADDRESS":
+		flag.StringVar(&opt.ServerAddr, "a", "localhost:8080", "address and port server")
+	case "POLL_INTERVAL":
+		flag.IntVar(&opt.PollInterval, "p", 2, "interval in seconds fof getting metrics")
+	case "REPORT_INTERVAL":
+		flag.IntVar(&opt.ReportInterval, "r", 10, "interval in seconds for sending metrics")
+	}
 }
 
 func ParseServerFlags() ServerOptions {
 	var options ServerOptions
-	flag.StringVar(&options.ServerAddr, "a", "localhost:8080", "address and port to run server on")
-	flag.Parse()
+	if err := env.Parse(&options); err != nil {
+		if errors.Is(err, env.ParseError{}) {
+			log.Printf("can't parse agent environment variables: %s", err.Error())
+		}
+		if errors.Is(err, env.VarIsNotSetError{}) {
+			aggErr := env.AggregateError{}
+			if ok := errors.As(err, &aggErr); ok {
+				for _, er := range aggErr.Errors {
+					switch v := er.(type) {
+					case env.VarIsNotSetError:
+						setServerFlagByName(&options, v.Key)
+					default:
+						fmt.Printf("Unknown error type %v", v)
+					}
+				}
+			}
+			flag.Parse()
+		}
+	}
+
 	return options
+}
+
+func setServerFlagByName(opt *ServerOptions, name string) {
+	switch name {
+	case "ADDRESS":
+		flag.StringVar(&opt.ServerAddr, "a", "localhost:8080", "address and port to run server on")
+	}
 }
