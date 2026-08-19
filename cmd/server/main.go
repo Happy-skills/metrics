9e395213bf3fd9e7ec7017325c9b08458bc0c001
Happy-skills <happy@johnatan.ru>
2026-08-19 10:28:07 +0300

    fix(server): stop with fatal on DB init error

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/Happy-skills/metrics/internal/config"
@@ -9,40 +10,45 @@ import (
 	"github.com/Happy-skills/metrics/internal/logger"
 	"github.com/Happy-skills/metrics/internal/repository"
 	"github.com/Happy-skills/metrics/internal/service"
+	"go.uber.org/zap"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	options := config.ParseServerFlags()
 
 	if err := logger.Initialize("info"); err != nil {
 		log.Fatal(err.Error())
 	}
 
-	var store repository.Storage
-
-	if options.DatabaseDSN != "" {
-		pgxPoll, err := db.InitializeDB(ctx, options.DatabaseDSN)
-		if err != nil {
-			log.Println(err.Error())
-		}
-
-		store = repository.NewDBStorage(pgxPoll)
-	} else {
-		store = repository.NewMemStorage(options.FileStoragePath, options.StoreInterval)
-
-		if options.RestoreOnStart && options.FileStoragePath != "" {
-			repository.LoadMetricsFromFile(options, store)
-		}
-
-		if options.FileStoragePath != "" && options.StoreInterval > 0 {
-			go service.WriteMetrics(options, store)
-		}
+	store, err := initStorage(options)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
 	if err := service.RunServer(options, store); err != nil {
-		logger.Log.Fatal(err.Error())
+		logger.Log.Fatal("unable to run server", zap.Error(err))
 	}
+}
+
+func initStorage(options config.ServerOptions) (repository.Storage, error) {
+	if options.DatabaseDSN != "" {
+		pgxPoll, err := db.InitializeDB(context.TODO(), options.DatabaseDSN)
+		if err != nil {
+			return nil, fmt.Errorf("unable to init DB: %w", err)
+		}
+
+		return repository.NewDBStorage(pgxPoll), nil
+	}
+
+	store := repository.NewMemStorage(options.FileStoragePath, options.StoreInterval)
+
+	if options.RestoreOnStart && options.FileStoragePath != "" {
+		repository.LoadMetricsFromFile(options, store)
+	}
+
+	if options.FileStoragePath != "" && options.StoreInterval > 0 {
+		go service.WriteMetrics(options, store)
+	}
+
+	return store, nil
 }
